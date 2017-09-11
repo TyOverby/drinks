e76fc6594266825a0179b292f3c576fbe3e8a0ea
Ty Overby <ty@pre-alpha.com>
2017-09-10 17:12:08 -0700

    misc update

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -74,6 +74,121 @@ module.exports = React;
 
 /***/ }),
 /* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__(0);
+const ReactDOM = __webpack_require__(7);
+const RecipeGrid_1 = __webpack_require__(5);
+const ControlPanel_1 = __webpack_require__(3);
+function get_rerenderer(recipies) {
+    return function (appprops) {
+        const nameFilter = (r) => {
+            if (appprops.nameSearch == "") {
+                return true;
+            }
+            if (r.name.toLowerCase().indexOf(appprops.nameSearch) !== -1) {
+                return true;
+            }
+            return false;
+        };
+        const ingredientFilter = (r) => {
+            if (appprops.ingredientSearch.length === 0) {
+                return true;
+            }
+            for (const singr of appprops.ingredientSearch) {
+                let searching = r.ingredients;
+                if (r.standard_garnish) {
+                    searching = searching.concat([r.standard_garnish]);
+                }
+                const contained = searching.some(i => i.toLowerCase().indexOf(singr) != -1);
+                if (!contained) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        const r2 = recipies.filter(nameFilter).filter(ingredientFilter);
+        ReactDOM.render(React.createElement("div", { id: "wrapper" },
+            React.createElement(ControlPanel_1.ControlPanel, Object.assign({}, appprops, { searchCount: r2.length, totalCount: recipies.length })),
+            React.createElement(RecipeGrid_1.RecipeGrid, { recipies: r2, app: appprops })), document.querySelector("#container"));
+    };
+}
+exports.get_rerenderer = get_rerenderer;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+function getRecipies() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return (yield fetch("./res/list.json")).json();
+    });
+}
+exports.getRecipies = getRecipies;
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__(0);
+class ControlPanel extends React.Component {
+    updateNameSearch(event) {
+        const value = event.target.value.toLowerCase().trim();
+        this.props.rerender({
+            nameSearch: value,
+            ingredientSearch: this.props.ingredientSearch,
+            rerender: this.props.rerender,
+        });
+    }
+    updateIngredientsSearch(event) {
+        const value = event.target.value.toLowerCase().split(",").map(s => s.trim());
+        this.props.rerender({
+            nameSearch: this.props.nameSearch,
+            ingredientSearch: value,
+            rerender: this.props.rerender,
+        });
+    }
+    render() {
+        return React.createElement("div", { id: "control" },
+            React.createElement("h1", null, " DRINKS "),
+            React.createElement("form", null,
+                React.createElement("div", { className: "search-group" },
+                    React.createElement("label", { htmlFor: "name-search" }, " \uD83D\uDD0D Names "),
+                    React.createElement("input", { id: "name-search", type: "text", onChange: this.updateNameSearch.bind(this) })),
+                React.createElement("div", { className: "search-group" },
+                    React.createElement("label", { htmlFor: "ingredient-search" }, " \uD83D\uDD0D Ingredients "),
+                    React.createElement("input", { id: "ingredient-search", type: "text", onChange: this.updateIngredientsSearch.bind(this) })),
+                React.createElement("div", null,
+                    "Showing ",
+                    this.props.searchCount,
+                    " of ",
+                    this.props.totalCount)));
+    }
+}
+exports.ControlPanel = ControlPanel;
+
+
+/***/ }),
+/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -128,48 +243,14 @@ exports.Recipe = Recipe;
 
 
 /***/ }),
-/* 2 */
-/***/ (function(module, exports) {
-
-module.exports = ReactDOM;
-
-/***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const model = __webpack_require__(7);
-const global_1 = __webpack_require__(9);
-(() => __awaiter(this, void 0, void 0, function* () {
-    const recipies = yield model.getRecipies();
-    const render = global_1.get_rerenderer(recipies);
-    render({
-        nameSearch: "",
-        ingredientSearch: [],
-        rerender: render,
-    });
-}))();
-
-
-/***/ }),
-/* 4 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = __webpack_require__(0);
-const Recipe_1 = __webpack_require__(1);
+const Recipe_1 = __webpack_require__(4);
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
 class RecipeGrid extends React.Component {
@@ -182,9 +263,7 @@ exports.RecipeGrid = RecipeGrid;
 
 
 /***/ }),
-/* 5 */,
-/* 6 */,
-/* 7 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -198,105 +277,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-function getRecipies() {
-    return __awaiter(this, void 0, void 0, function* () {
-        return (yield fetch("./res/list.json")).json();
+const model = __webpack_require__(2);
+const global_1 = __webpack_require__(1);
+(() => __awaiter(this, void 0, void 0, function* () {
+    const recipies = yield model.getRecipies();
+    const render = global_1.get_rerenderer(recipies);
+    render({
+        nameSearch: "",
+        ingredientSearch: [],
+        rerender: render,
     });
-}
-exports.getRecipies = getRecipies;
+}))();
 
 
 /***/ }),
-/* 8 */
-/***/ (function(module, exports, __webpack_require__) {
+/* 7 */
+/***/ (function(module, exports) {
 
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = __webpack_require__(0);
-class ControlPanel extends React.Component {
-    updateNameSearch(event) {
-        const value = event.target.value.toLowerCase().trim();
-        this.props.rerender({
-            nameSearch: value,
-            ingredientSearch: this.props.ingredientSearch,
-            rerender: this.props.rerender,
-        });
-    }
-    updateIngredientsSearch(event) {
-        const value = event.target.value.toLowerCase().split(",").map(s => s.trim());
-        this.props.rerender({
-            nameSearch: this.props.nameSearch,
-            ingredientSearch: value,
-            rerender: this.props.rerender,
-        });
-    }
-    render() {
-        return React.createElement("div", { id: "control" },
-            React.createElement("h1", null, " DRINKS "),
-            React.createElement("form", null,
-                React.createElement("div", { className: "search-group" },
-                    React.createElement("label", { htmlFor: "name-search" }, " \uD83D\uDD0D Names "),
-                    React.createElement("input", { id: "name-search", type: "text", onChange: this.updateNameSearch.bind(this) })),
-                React.createElement("div", { className: "search-group" },
-                    React.createElement("label", { htmlFor: "ingredient-search" }, " \uD83D\uDD0D Ingredients "),
-                    React.createElement("input", { id: "ingredient-search", type: "text", onChange: this.updateIngredientsSearch.bind(this) })),
-                React.createElement("div", null,
-                    "Showing ",
-                    this.props.searchCount,
-                    " of ",
-                    this.props.totalCount)));
-    }
-}
-exports.ControlPanel = ControlPanel;
-
-
-/***/ }),
-/* 9 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = __webpack_require__(0);
-const ReactDOM = __webpack_require__(2);
-const RecipeGrid_1 = __webpack_require__(4);
-const ControlPanel_1 = __webpack_require__(8);
-function get_rerenderer(recipies) {
-    return function (appprops) {
-        const nameFilter = (r) => {
-            if (appprops.nameSearch == "") {
-                return true;
-            }
-            if (r.name.toLowerCase().indexOf(appprops.nameSearch) !== -1) {
-                return true;
-            }
-            return false;
-        };
-        const ingredientFilter = (r) => {
-            if (appprops.ingredientSearch.length === 0) {
-                return true;
-            }
-            for (const singr of appprops.ingredientSearch) {
-                let searching = r.ingredients;
-                if (r.standard_garnish) {
-                    searching = searching.concat([r.standard_garnish]);
-                }
-                const contained = searching.some(i => i.toLowerCase().indexOf(singr) != -1);
-                if (!contained) {
-                    return false;
-                }
-            }
-            return true;
-        };
-        const r2 = recipies.filter(nameFilter).filter(ingredientFilter);
-        ReactDOM.render(React.createElement("div", { id: "wrapper" },
-            React.createElement(ControlPanel_1.ControlPanel, Object.assign({}, appprops, { searchCount: r2.length, totalCount: recipies.length })),
-            React.createElement(RecipeGrid_1.RecipeGrid, { recipies: r2, app: appprops })), document.querySelector("#container"));
-    };
-}
-exports.get_rerenderer = get_rerenderer;
-
+module.exports = ReactDOM;
 
 /***/ })
 /******/ ]);
