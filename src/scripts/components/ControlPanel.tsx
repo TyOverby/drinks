f5a02424597406d50d27c3e0f57d7047380c5d9e
Ty Overby <ty@pre-alpha.com>
2017-10-04 23:02:31 -0700

    more style

--- src/scripts/components/ControlPanel.tsx
+++ src/scripts/components/ControlPanel.tsx
@@ -17,7 +17,7 @@ export class ControlPanel extends React.Component<ControlPanelProps> {
     }
 
     updateIngredientsSearch(event: React.ChangeEvent<HTMLInputElement>) {
-        const value =event.target.value.toLowerCase().split(",").map(s => s.trim());
+        const value = event.target.value.toLowerCase().split(",").map(s => s.trim());
         this.props.rerender({
             nameSearch: this.props.nameSearch,
             ingredientSearch: value,
@@ -28,19 +28,19 @@ export class ControlPanel extends React.Component<ControlPanelProps> {
     render() {
         return <div id="control">
             <h1> Drinks</h1>
-                <div className="search-group">
-                    <label htmlFor="name-search"> <span className="search-specifier"></span>&nbsp;Names </label>
-                    <input id="name-search" placeholder="martini" type="text" onChange={this.updateNameSearch.bind(this)}></input>
-                </div>
+            <div className="search-group">
+                <label htmlFor="name-search"> <span className="search-specifier"></span>&nbsp;Names </label>
+                <input id="name-search" placeholder='"martini"' type="text" onChange={this.updateNameSearch.bind(this)}></input>
+            </div>
 
-                <div className= "search-group">
-                    <label htmlFor="ingredient-search"> <span className="search-specifier"></span>&nbsp;Ingredients </label>
-                    <input id="ingredient-search" placeholder="rum" type="text" onChange={this.updateIngredientsSearch.bind(this)}></input>
-                </div>
+            <div className="search-group">
+                <label htmlFor="ingredient-search"> <span className="search-specifier"></span>&nbsp;Ingredients </label>
+                <input id="ingredient-search" placeholder='"rum"' type="text" onChange={this.updateIngredientsSearch.bind(this)}></input>
+            </div>
 
-                <div id = "showing">
-                    Showing {this.props.searchCount} of {this.props.totalCount}
-                </div>
+            <div id="showing">
+                Showing {this.props.searchCount} of {this.props.totalCount}
+            </div>
         </div>;
     }
 }
